4724dd445e8aa308419f91554160506316fcd030
jenny-kwan <jennykwan222@gmail.com>
2025-04-15 11:03:47 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -45,14 +45,14 @@ public class Sound
   {
     /* to be implemented in part (b) */
     int n = 0;
-    while (samples[n] == 0)
+    while (samples[n] == 0) {
       n++
-
+    }
     int[] newSamples = new int[samples.length - n];
 
-    for (int i = 0; i < newSamples.length; i++)
+    for (int i = 0; i < newSamples.length; i++){
       newSamples[i] = samples[i+n];
-
+    }
     samples = newSamples;
   }
 }
